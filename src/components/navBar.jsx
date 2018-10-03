e576b42220412e4b0361cfc10870d22f4920e573
Tim Williams <jtimwill@gmail.com>
2018-10-03 15:30:08 -0400

    Add muscle map

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,66 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { getCurrentUser } from '../services/authService';
 
-const NavBar = () => {
-  const user = getCurrentUser();
+class NavBar extends Component {
+  state = {
+    show: false
+  };
 
+  handleToggler = () => {
+    const show = !this.state.show;
+    this.setState({show});
+  };
 
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <Link className="navbar-brand" to="/">WorkoutLogger</Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <div className="navbar-nav">
-          {!user && (
-            <React.Fragment>
-              <NavLink className="nav-item nav-link" to="/login">
-                Login
-              </NavLink>
-              <NavLink className="nav-item nav-link" to="/users/new">
-                Register
-              </NavLink>
-            </React.Fragment>
-          )}
-          {user && (
-            <React.Fragment>
-              <NavLink className="nav-item nav-link" to="/logout">
-                Logout
-              </NavLink>
-              <NavLink className="nav-item nav-link" to="/users/me/show">
-                Profile
-              </NavLink>
-              <NavLink className="nav-item nav-link" to="/workouts/index">
-                Workouts
-              </NavLink>
-              <NavLink className="nav-item nav-link" to="/exercises/index">
-                Exercises
-              </NavLink>
-              <NavLink className="nav-item nav-link" to="/muscles/index">
-                Muscles
-              </NavLink>
-            </React.Fragment>
-          )}
-          {user && user.admin && (
-            <React.Fragment>
-              <NavLink className="nav-item nav-link" to="/users/index">
-                Users
-              </NavLink>
-            </React.Fragment>
-          )}
+  render() {
+    const user = getCurrentUser();
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <Link className="navbar-brand" to="/">WorkoutLogger</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={this.handleToggler}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="navbar-collapse"
+          id="navbarNav"
+          display={this.state.show.toString()}
+          >
+          <div className="navbar-nav">
+            {!user && (
+              <React.Fragment>
+                <NavLink className="nav-item nav-link" to="/login">
+                  Login
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/users/new">
+                  Register
+                </NavLink>
+              </React.Fragment>
+            )}
+            {user && (
+              <React.Fragment>
+                <NavLink className="nav-item nav-link" to="/logout">
+                  Logout
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/users/me/show">
+                  Profile
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/workouts/index">
+                  Workouts
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/exercises/index">
+                  Exercises
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/muscles/index">
+                  Muscles
+                </NavLink>
+              </React.Fragment>
+            )}
+            {user && user.admin && (
+              <React.Fragment>
+                <NavLink className="nav-item nav-link" to="/users/index">
+                  Users
+                </NavLink>
+              </React.Fragment>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  }
+}
 
 export default NavBar;
